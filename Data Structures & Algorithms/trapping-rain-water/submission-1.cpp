2fb2e942d3c53d381209e3b7a47f5e6f6154c3e7
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-06 02:15:01 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+
+        int l = 0, r = n-1;
+        int lmax = 0, rmax = 0;
+        int ans = 0;
+
+        while (l<r){
+            if (h[l]<h[r]){
+                if (h[l]>lmax) lmax = h[l];
+                else ans += lmax-h[l];
+                l++;
+            }
+            else{
+                if (h[r]>rmax) rmax = h[r];
+                else ans += rmax-h[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
