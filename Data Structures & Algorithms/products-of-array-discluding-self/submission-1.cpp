512cf6a7515ce085cdf1e09333d172a41e7143ad
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-05 21:20:43 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> output(n);
+
+        vector<int> prefix(n);
+        prefix[0] = nums[0];
+        for (int i = 1; i<n; i++){
+            prefix[i] = prefix[i-1]*nums[i];
+        }
+
+        vector<int> suffix(n);
+        suffix[n-1] = nums[n-1];
+        for (int i = n-2; i>=0; i--){
+            suffix[i] = suffix[i+1]*nums[i];
+        }
+
+        output[0] = suffix[1];
+        output[n-1] = prefix[n-2];
+        for (int i = 1; i<n-1; i++){
+            output[i] = prefix[i-1]*suffix[i+1];
+        }
+        return output;
+    }
+};
