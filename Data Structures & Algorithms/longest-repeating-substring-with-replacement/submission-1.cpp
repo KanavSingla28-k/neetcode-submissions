ec76b07c79a404e0eceb22fa0ebb424e9629ead9
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-07 12:40:29 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+
+        int l = 0;
+        int ans = 0;
+
+        vector<int> freq(26, 0);
+        int maxfreq = 0;
+
+        for (int r = 0; r<n; r++){
+            freq[s[r]-'A']++;
+            maxfreq = max(maxfreq, freq[s[r]-'A']);
+
+            while (r-l+1 - maxfreq > k){
+                freq[s[l++]-'A']--;
+            }
+
+            
+            ans = max(ans, r-l+1);
+        }
+        return ans;
+    }
+};
