2abf6f5ef18f84f70cc26d3f56f79d958b8e34a5
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-07 12:08:59 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        int rmax = -1;
+        vector<int> ans(n);
+
+        for (int i = n-1; i>=0; i--){
+            if (i == n-1) {
+                ans[i] = -1;
+                rmax = prices[i];
+            }
+
+            if (prices[i]>=rmax){
+                rmax = prices[i];
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = rmax-prices[i];
+            }
+        }
+
+        int a = *max_element(ans.begin(), ans.end());
+
+        return (a == -1) ? 0: a;
+    }
+};
