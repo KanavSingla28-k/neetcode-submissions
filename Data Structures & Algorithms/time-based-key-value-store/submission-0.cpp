2738f4cc51e43e8e5412cd95d4c9de703416e5ac
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-08 23:13:42 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,34 @@
+class TimeMap {
+public:
+unordered_map<string, vector<pair<int, string>>> mp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (!mp.count(key)) return "";
+
+        vector<pair<int, string>>& v = mp[key];
+
+        string ans = "";
+
+        int l = 0, r = v.size()-1;
+
+        while (l<=r){
+            int mid = l+(r-l)/2;
+
+            if (v[mid].first<=timestamp){
+                ans = v[mid].second;
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+};
