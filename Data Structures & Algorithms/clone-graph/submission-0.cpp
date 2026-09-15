c68f99abdca3dfc7250f4a606603ed126649d8db
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-15 15:47:30 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        unordered_map<Node*, Node*> mp;
+
+        Node* copy = new Node(node->val, {});
+        mp[node] = copy;
+
+        queue<Node*> q;
+        q.push(node);
+
+        while (!q.empty()){
+            Node* curr = q.front(); q.pop();
+
+            for (Node* n: curr->neighbors){
+                if (!mp.count(n)){
+                    mp[n] = new Node(n->val, {});
+                    q.push(n);
+                }
+                mp[curr]->neighbors.push_back(mp[n]);
+            }         
+        }
+        return copy;
+    }
+};
