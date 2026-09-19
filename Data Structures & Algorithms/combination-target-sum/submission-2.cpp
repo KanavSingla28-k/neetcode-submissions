c04a21b5f1721042930834b9b071a361f839d862
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-19 19:44:46 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    set<vector<int>> seen;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        vector<int> curr;
+
+        solve(nums, target, res, curr, 0);
+
+        return res;
+    }
+
+    void solve(vector<int>& nums, int target, vector<vector<int>>& res, vector<int>& curr, int i){
+        if (target < 0 || i == nums.size()) return;
+        if (target == 0){
+            if (!seen.count(curr)){
+                res.push_back(curr);
+                seen.insert(curr);
+            }
+            return;
+        }
+
+        curr.push_back(nums[i]);
+        solve(nums, target-nums[i], res, curr, i);
+
+        curr.pop_back();
+        solve(nums, target, res, curr, i+1);
+    }
+};
