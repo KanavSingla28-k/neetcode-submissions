44c6e416bde87020138668ed9ad43bafdda18765
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-09-21 16:39:14 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return (nums[0]>nums[1]) ? nums[0]: nums[1];
+        vector<int> dp(n, 0);
+
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[0]+nums[2];
+
+        for (int i = 3; i<n; i++){
+            dp[i] = max(dp[i-3], dp[i-2])+nums[i];
+        }
+
+        return (dp[n-1]> dp[n-2]) ? dp[n-1]: dp[n-2];
+    }
+};
